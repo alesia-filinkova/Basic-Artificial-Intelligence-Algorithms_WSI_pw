586d8d27a1de1753781e66bb7a368bf8f8778960
afilinko <01194269@pw.edu.pl>
2024-10-23 16:15:28 +0200

    made functions gradient() and grad_descent() that work with function f, now refactore them to work also with function g

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -1,29 +1,72 @@
 import sys
 import numpy as np
+import sympy as sp
 import matplotlib.pyplot as plt
-
-EPSILON = 1e-6
-
-
-def function_f(A, B):
-    UPPER_LIMIT = np.pi * 4
-    x = np.random.uniform(-UPPER_LIMIT, UPPER_LIMIT)
-    print(f(x, A, B), x, A, B)
-    print(derivate_f(x, A, B), x, A, B)
 
 
 def f(x, A, B):
-    return A * x + B * np.sin(x)
+    return A * x + B * sp.sin(x)
 
 
-def gradient_f(x, A, B):
-    # df_dx = A + B * np.cos(x)
-    
-    return df_dx
-
-
-def function_g(x, y, C):
+def g(x, y, C):
     return (C * x * y) / (np.exp(x**2) + y**2)
+
+
+def function_f(A, B):
+    UPPER_BOUND = 4 * np.pi
+    learning_rate_f = 0.001
+    max_steps_f = 10000
+    start_point_f = np.random.uniform(-UPPER_BOUND, UPPER_BOUND)
+    extremum_f, history_f = grad_descent(f, gradient,
+                                         [start_point_f], learning_rate_f,
+                                         max_steps_f, UPPER_BOUND, A, B)
+    print(extremum_f, history_f[0])
+
+
+def function_g(C):
+    UPPER_BOUND = 2
+    learning_rate_g = 0.001
+    max_steps_g = 10000
+    start_point_g = np.random.uniform(-UPPER_BOUND, UPPER_BOUND, size=2)
+    extremum_g, history_g = grad_descent(g, gradient,
+                                         [start_point_g],
+                                         learning_rate_g, max_steps_g,
+                                         UPPER_BOUND, C)
+    print(extremum_g, history_g[0])
+
+
+def gradient(func, symbols, *args):
+    if type(args[1]) is int:
+        grad_fct = sp.diff(f(symbols[0], args[1], args[2]), symbols[0])
+        grad = grad_fct.subs(symbols[0], args[0])
+        # print(grad)
+        return float(grad)
+    elif type(args[1]) is not int:
+        grad_fct_x, grad_fct_y = sp.diff(g(symbols[0], args[-1]), symbols[0])
+        grad_x, grad_y = grad_fct.subs(symbols[0], args[0])
+        return [grad_x, grad_y]
+    else:
+        raise ValueError("The numbers of values can be only 1 or 2")
+
+
+def grad_descent(func, grad_func, start_point, learning_rate, max_steps,
+                 UPPER_BOUND, *args):
+    x_symb, y_symb = sp.symbols('x y')
+    symbols = [x_symb, y_symb]
+    point = np.array(start_point)
+    history = [point.copy()]
+    count_it = 0
+    print(*point)
+    grad = grad_func(func, symbols, *point, *args)
+    while count_it < max_steps and np.linalg.norm(grad) > 1e-6:
+        # print(*point)
+        grad = grad_func(func, symbols, *point, *args)
+        point = point - learning_rate * grad
+        point = np.clip(point, -UPPER_BOUND, UPPER_BOUND)
+        history.append(point.copy())
+        count_it += 1
+
+    return point, history
 
 
 def main():
@@ -34,6 +77,7 @@ def main():
     index = sys.argv[1]
     A, B, C = index[-3:][::-1]
     function_f(int(A), int(B))
+    function_g(int(C))
 
 
 if __name__ == "__main__":

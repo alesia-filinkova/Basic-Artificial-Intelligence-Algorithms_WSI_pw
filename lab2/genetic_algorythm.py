0de12a50306437dae0ce943f86131f7602bd10fc
afilinko <01194269@pw.edu.pl>
2024-11-07 11:15:59 +0100

    created class EvolutionAlgorithm and method __init__ in it that get and init all values that are needed to start algorithm; created method create_population() (now is empty) cause need self.population in init

---
+++ lab2/genetic_algorythm.py
@@ -0,0 +1,12 @@
+
+
+class EvolutionAlgorithm:
+    def __init__(self, cities_matrix, population_size=100, generations=100, mutation_rate=0.01):
+        self.cities_matrix = cities_matrix
+        self.population_size = population_size
+        self.generations = generations
+        self.mutation_rate = mutation_rate
+        self.population = self.create_population()
+
+    def create_population(self):
+        return

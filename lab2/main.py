14215f10bd18575b725634432f6aae620d148ce0
afilinko <01194269@pw.edu.pl>
2024-11-10 15:41:58 +0100

    added call of EvolutionAlgorithm and print results

--- lab2/main.py
+++ lab2/main.py
@@ -4,18 +4,14 @@ import pathlib
 import numpy as np
 import pandas as pd
 from solution_utils import generate_solution, decode_solution, evaluate_solution
+from genetic_algorythm import EvolutionAlgorithm
 
 MINI_CITIES_NUM = 5
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--cities-path",
-        type=pathlib.Path,
-        required=True,
-        help="Path to cities csv file",
-    )
+    parser.add_argument("--cities-path", type=pathlib.Path, required=True, help="Path to cities csv file")
     parser.add_argument(
         "--problem-size",
         choices=["mini", "full"],
@@ -30,19 +26,13 @@ def parse_args():
 
 def load_data(args):
     data = pd.read_csv(args.cities_path)
-    data_without_start_and_finish = data[
-        ~((data.index == args.finish) | (data.index == args.start))
-    ]
+    data_without_start_and_finish = data[~((data.index == args.finish) | (data.index == args.start))]
     if args.problem_size == "mini":
         city_names = (
-            [args.start]
-            + data_without_start_and_finish.sample(n=MINI_CITIES_NUM - 2).index.tolist()
-            + [args.finish]
+            [args.start] + data_without_start_and_finish.sample(n=MINI_CITIES_NUM - 2).index.tolist() + [args.finish]
         )
     else:
-        city_names = (
-            [args.start] + data_without_start_and_finish.index.tolist() + [args.finish]
-        )
+        city_names = [args.start] + data_without_start_and_finish.index.tolist() + [args.finish]
 
     return data[city_names].loc[city_names]
 
@@ -53,11 +43,17 @@ def main():
         np.random.seed(args.seed)
 
     data = load_data(args)
-    # print(data)
     print(generate_solution(data))
     print(decode_solution(data, generate_solution(data)))
-    distances_random = evaluate_solution(data, generate_solution(data))
-    print(distances_random)
+    distances_r = evaluate_solution(data, generate_solution(data))
+    print(distances_r)
+    ea = EvolutionAlgorithm(data)
+    best_route = ea.run()
+    print(best_route)
+    best_route_cities = decode_solution(data, best_route)
+    total_distance = evaluate_solution(data, best_route)
+    print("Best route:", best_route_cities)
+    print("Best route length:", total_distance)
 
 
 if __name__ == "__main__":

5ef57a7cd79189a8c765fd5f7d7806d2a09b1479
afilinko <01194269@pw.edu.pl>
2024-12-12 00:18:55 +0100

    replace M and B in y to 1 and 0 and X and y now matrixes

--- lab4/main.py
+++ lab4/main.py
@@ -1,8 +1,14 @@
 from ucimlrepo import fetch_ucirepo
+import pandas as pd
 
 breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
 
 X = breast_cancer_wisconsin_diagnostic.data.features
 y = breast_cancer_wisconsin_diagnostic.data.targets
 
+pd.set_option("future.no_silent_downcasting", True)
+y = y.replace({"M": 1, "B": 0}).astype(int)
 
+
+X = X.to_numpy()
+y = y.to_numpy()

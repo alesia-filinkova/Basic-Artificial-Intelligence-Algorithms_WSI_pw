f65ea83fb16020bb9fce31461693d20256557c45
afilinko <01194269@pw.edu.pl>
2024-12-12 00:20:09 +0100

    normalize X and separate data to train and test

--- lab4/main.py
+++ lab4/main.py
@@ -1,5 +1,7 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
 
@@ -12,3 +14,14 @@ y = y.replace({"M": 1, "B": 0}).astype(int)
 
 X = X.to_numpy()
 y = y.to_numpy()
+
+# normalization of data
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+# X = (X - X.mean(axis=0)) / X.std(axis=0)
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42
+)
+

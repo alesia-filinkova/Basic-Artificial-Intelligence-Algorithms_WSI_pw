78b060f44b3767a4cf8fd74b0819f2472cbe8245
alesia-filinkova <alesya.filinkova@gmail.com>
2024-12-12 00:27:42 +0100

    create model that will be our LogisticRegression algorythm, call function fit to train algorithm and then call predict and predict_proba to check how good algorithm work

--- lab4/main.py
+++ lab4/main.py
@@ -1,6 +1,8 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from logistic_regression import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
 breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
@@ -20,8 +22,15 @@ scaler = StandardScaler()
 X = scaler.fit_transform(X)
 # X = (X - X.mean(axis=0)) / X.std(axis=0)
 
+# X.
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=42
 )
 
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+y_pred_proba = model.predict_proba(X_test)

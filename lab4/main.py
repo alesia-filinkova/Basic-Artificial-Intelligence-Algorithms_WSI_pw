164455ade1fbb8080bb0e26bd2e78bbcf851c874
afilinko <01194269@pw.edu.pl>
2024-12-12 00:29:09 +0100

    call accuracy_score, f1_score, roc_auc_score to check algorithm

--- lab4/main.py
+++ lab4/main.py
@@ -32,5 +32,12 @@ model = LogisticRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
 y_pred_proba = model.predict_proba(X_test)
+roc_auc = roc_auc_score(y_test, y_pred_proba)
+
+print(f"Accuracy: {accuracy:.2f}")
+print(f"F1 Score: {f1:.2f}")
+print(f"AUROC: {roc_auc:.2f}")

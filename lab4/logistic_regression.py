145dfa63c502f0a6ffd303a533607d7f8f5ccd50
afilinko <01194269@pw.edu.pl>
2024-12-12 00:22:30 +0100

    create class LogisticRegression with functions that will be needed that now are empty

---
+++ lab4/logistic_regression.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, iteration_count=1000):
+        self.learning_rate = learning_rate
+        self.iteration_count = iteration_count
+        self.weights = None
+
+    def sigmoid(self, z):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def predict_proba(self, X):
+        pass

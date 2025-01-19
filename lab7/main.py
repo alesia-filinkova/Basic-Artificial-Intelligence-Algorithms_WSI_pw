89241bdd15981147bc54ded8558fd5a320505117
afilinko <01194269@pw.edu.pl>
2025-01-19 16:23:07 +0100

    prepare data

--- lab7/main.py
+++ lab7/main.py
@@ -1,6 +1,15 @@
-import kagglehub
+import bnlearn as bn
+import pandas as pd
 
-# Download latest version
-path = kagglehub.dataset_download("mrayushagrawal/us-crime-dataset")
+url = "US_Crime_DataSet.csv"
+data = pd.read_csv(url)
 
-print("Path to dataset files:", path)
+target_columns = [
+    "Victim Sex", "Victim Age", "Victim Race",
+    "Perpetrator Sex", "Perpetrator Age", "Perpetrator Race",
+    "Relationship", "Weapon"
+]
+data = data[target_columns]
+
+data = data[~data.isin(["Unknown"]).any(axis=1)]
+

64b76d47cbc8ae1552992281e2104c06f519ad14
alesia-filinkova <alesya.filinkova@gmail.com>
2025-09-28 23:47:52 +0200

    rewrite implementation of MultilayerPerceptron to use Perceprton

--- lab5/mlp.py
+++ lab5/mlp.py
@@ -1,83 +1,68 @@
+from perceptron import Perceptron
 import numpy as np
-
-def relu(z):
-    return np.maximum(z, 0)
-
-def relu_derivative(z):
-    return 1 * (z > 0)
-
 
 class MultilayerPerceptron:
 
-    def __init__(self, layers_sizes, regularization):
-        self.layers_sizes = layers_sizes
-        self.layers = len(layers_sizes)
-        self.reg_lambda = regularization
+    def __init__(self, layer_sizes, activations, learning_rate=0.01):
+        """
+        layer_sizes: [11, 32, 16, 1]
+        activations: ["relu", "relu", "linear"]
+        """
+        self.layers = []
+        self.learning_rate = learning_rate
+        
+        for i in range(1, len(layer_sizes)):
+            layer = [Perceptron(layer_sizes[i-1], activation=activations[i-1])
+                     for _ in range(layer_sizes[i])]
+            self.layers.append(layer)
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = np.array([neuron.forward(x) for neuron in layer])
+        return x
+    
+    def backward(self, y_true, y_pred):
+        # gradient for MSE
+        grad = y_pred - y_true
+        for layer in reversed(self.layers):
+            new_grad = np.zeros(len(layer[0].input))
+            for i, neuron in enumerate(layer):
+                new_grad += neuron.backward(grad[i], self.learning_rate)
+            grad = new_grad
+    
+    def train(self, X, y, X_val=None, y_val=None, epochs=100):
+        history = {"train_loss": [], "val_loss": []}
+        
+        for epoch in range(epochs):
+            total_loss = 0
+            for xi, yi in zip(X, y):
+                y_pred = self.forward(xi)
+                loss = np.mean((yi - y_pred) ** 2)
+                total_loss += loss
+                self.backward(yi, y_pred)
+                
+            avg_train_loss = total_loss / len(X)
+            history["train_loss"].append(avg_train_loss)
+            
+            if X_val is not None and y_val is not None:
+                y_val_pred = self.predict(X_val)
+                val_loss = np.mean((y_val - y_val_pred) ** 2)
+                history["val_loss"].append(val_loss)
+            
+            if epoch % 10 == 0:
+                if X_val is not None:
+                    print(f"Epoch {epoch}, train_loss={avg_train_loss:.4f}, val_loss={val_loss:.4f}")
+                else:
+                    print(f"Epoch {epoch}, train_loss={avg_train_loss:.4f}")
 
-        self.initialise_weights()
-
-    def initialise_weights(self):
-        self.theta_weights = []
-
-        for layer in range(self.layers - 1):
-            current_layer_size = self.layers_sizes[layer]
-            next_layer_size = self.layers_sizes[layer + 1]
-
-            standard = np.sqrt(2.0 / current_layer_size)
-            thetas = np.random.randn(next_layer_size, current_layer_size + 1) * standard
-
-            self.theta_weights.append(thetas)
-
-        return self.theta_weights
-
-    def backpropagation(self, X, Y):
-        activated_dz = lambda x: relu_derivative(x)
-
-        n_samples = X.shape[0]
-        A, Z = self.feedforward(X)
-
-        deltas = [None] * self.layers
-        deltas[-1] = A[-1] - Y
-
-        for layer in range(self.layers - 2, 0, -1):
-            theta = self.theta_weights[layer][:, 1:]
-            deltas[layer] = (deltas[layer + 1] @ theta) * activated_dz(Z[layer])
-
-        gradients = []
-        for layer in range(self.layers - 1):
-            input_layer = A[layer]
-            gradient = deltas[layer + 1].T @ input_layer / n_samples
-            gradient[:, 1:] += (self.reg_lambda * self.theta_weights[layer][:, 1:]) / n_samples
-            gradients.append(gradient)
-
-        return gradients
-
-    def feedforward(self, X):
-        A = [None] * self.layers
-        Z = [None] * self.layers
-        input_layer = X
-
-        for layer in range(self.layers - 1):
-            n_samples = input_layer.shape[0]
-            input_layer = np.concatenate((np.ones([n_samples, 1]), input_layer), axis=1)
-            A[layer] = input_layer
-            Z[layer + 1] = np.matmul(input_layer, self.theta_weights[layer].transpose())
-
-            output_layer = relu(Z[layer + 1])
-
-            input_layer = output_layer
-
-        A[-1] = output_layer  # linear activation
-
-        return A, Z
-
-    def train(self, X, Y, iterations, learning_rate=0.01):
-        for iteration in range(iterations):
-            gradients = self.backpropagation(X, Y)
-            for i in range(len(self.theta_weights)):
-                self.theta_weights[i] -= learning_rate * gradients[i]
-
+        return history
+        
     def predict(self, X):
-        A, Z = self.feedforward(X)
-        Y_pred = A[-1]
-        return Y_pred
+        predictions = []
+        for xi in X:
+            y_pred = self.forward(xi)
+            if len(y_pred) == 1:
+                y_pred = y_pred[0]
+            predictions.append(y_pred)
+        return np.array(predictions)
+        
